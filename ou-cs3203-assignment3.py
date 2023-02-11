c17b1fc3e28210fcf96ed26bbe621158db00e419
Colton Gray <coltongray105@gmail.com>
2023-02-10 19:46:42 -0600

    Step 2: Added function to calculate product of a list

--- ou-cs3203-assignment3.py
+++ ou-cs3203-assignment3.py
@@ -4,3 +4,8 @@ def calculateSum(numbers):
         sum = sum + x
     return sum
 
+def calculateProduct(numbers):
+    product = 0
+    for x in numbers:
+        product = product * x
+    return product
