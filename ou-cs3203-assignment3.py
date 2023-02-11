9ebd2b0f27da3820dde75873f2f245382e635ca4
Colton Gray <coltongray105@gmail.com>
2023-02-10 19:54:48 -0600

    Step 3: Added main function

--- ou-cs3203-assignment3.py
+++ ou-cs3203-assignment3.py
@@ -5,7 +5,18 @@ def calculateSum(numbers):
     return sum
 
 def calculateProduct(numbers):
-    product = 0
+    product = 1
     for x in numbers:
         product = product * x
     return product
+
+print("Hello User! \nThis program will calculate the sum & product of a list of numbers you enter. \nThis list will be 5 integers long.\n")
+
+numberList = []
+for x in range(5):
+    number = int(input("Please enter a number for your list: "))
+    numberList.append(number)
+
+print("The sum is " + str(calculateSum(numberList)) + ".")
+print("The product is " + str(calculateProduct(numberList)) + ".")
+    
